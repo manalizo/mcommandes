eb7379fe5ac16195af263ac3d3268c30c59666fe
manalizo <yumimanal8@gmail.com>
2024-12-18 19:24:02 +0100

    initial commit

---
+++ src/test/java/com/mcommandes/McommandesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcommandes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McommandesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
